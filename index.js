beaecb8c4498df88948b0a43e198cc339266d0c7
Krishn Jatav <164301111+krishn1122@users.noreply.github.com>
2026-05-22 06:14:11 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("hello to CodeSpaces");
-
+const app= require('./app');
+const {PORT} = process.env;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+process.env.PORT = 3000;
+app.listen(3000, () => {
